577a7799685de732b5ea9fc39ca5626b12fe6a14
Ihor Banadiga <banadiga@gmail.com>
2024-05-01 17:20:23 +0300

    beanmatchers

---
+++ beanmatchers/src/test/java/ua/lviv/javaclub/beanmatchers/UserDTOTest.java
@@ -0,0 +1,27 @@
+package ua.lviv.javaclub.beanmatchers;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static com.google.code.beanmatchers.BeanMatchers.isABeanWithValidGettersAndSetters;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class UserDTOTest {
+
+  @Test
+  void test() {
+    assertThat(UserDTO.class, allOf(
+        hasValidBeanConstructor(),
+        hasValidBeanEquals(),
+        hasValidGettersAndSetters(),
+        isABeanWithValidGettersAndSetters(),
+        hasValidBeanHashCode(),
+        hasValidBeanToString()));
+  }
+} 
